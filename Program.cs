0b57bccee8d6fd103aaf93c146a14f0a5bdfd868
Apsal <165996044+k-Afsal@users.noreply.github.com>
2025-06-17 18:16:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Computer
+    {
+        public string Motherboard {get; set;}
+        public int CPUCores {get; set;}
+        public bool HasWifi {get; set;}
+        public bool HasLTE {get; set;}
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+        public string VideoCard {get; set;}
+        
+        public Computer()
+        {
+            if (Motherboard == null)
+            {
+                Motherboard = "";
+            }
+            if (VideoCard == null)
+            {
+                VideoCard = "";
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Computer myComputer = new Computer()
+             {
+                    Motherboard = "ASUS ROG Strix",
+                    CPUCores = 8,
+                    HasWifi = true,
+                    HasLTE = false,
+                    ReleaseDate = new DateTime(2023, 10, 1),
+                    Price = 1499.99m,
+                    VideoCard = "NVIDIA GeForce RTX 3080"
+             };
+        }
+    }                            
+}
